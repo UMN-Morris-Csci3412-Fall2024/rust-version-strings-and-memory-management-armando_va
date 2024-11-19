669a3095284f714641855478d7f592f644fd10c7
Armando Valdez <155396846+valde148@users.noreply.github.com>
2024-11-19 15:59:44 -0600

    Update main.rs

--- disemvowel/main.rs
+++ disemvowel/main.rs
@@ -62,7 +62,7 @@ mod tests {
 
     #[test]
     fn requires_two_arguments() {
-        let mut cmd = Command::cargo_bin("disemvowel-in-rust").unwrap();
+        let mut cmd = Command::cargo_bin("disemvowel").unwrap();
         cmd.arg("1");
         cmd.assert()
             .failure()
@@ -71,7 +71,7 @@ mod tests {
 
     #[test]
     fn requires_read_file() {
-        let mut cmd = Command::cargo_bin("disemvowel-in-rust").unwrap();
+        let mut cmd = Command::cargo_bin("disemvowel").unwrap();
         cmd.arg("/this/path/does/not/exist")
             .arg("output/path/does/not/matter");
         cmd.assert()
